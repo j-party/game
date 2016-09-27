6ee93ee2222d0f93d6d621a8a39b8dfeb6debc14
Eric Heikes <eheikes@gmail.com>
2016-09-26 21:48:38 -0500

    Delete unused Clues class.

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -248,42 +248,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-define('clues',["require", "exports", 'aurelia-framework', 'aurelia-router', './clue-service'], function (require, exports, aurelia_framework_1, aurelia_router_1, clue_service_1) {
-    "use strict";
-    var Clues = (function () {
-        function Clues(clueService, router) {
-            this.clueService = clueService;
-            this.router = router;
-        }
-        Clues.prototype.getClues = function () {
-            var _this = this;
-            this.clueService.getClues().then(function (clues) { return _this.clues = clues; });
-        };
-        Clues.prototype.created = function () {
-            this.getClues();
-        };
-        Clues.prototype.onSelect = function (clue) { this.selectedClue = clue; };
-        Clues.prototype.gotoDetail = function () {
-            this.router.navigateToRoute('clueDetail', { id: this.selectedClue.id });
-        };
-        Clues = __decorate([
-            aurelia_framework_1.inject(clue_service_1.ClueService, aurelia_router_1.Router), 
-            __metadata('design:paramtypes', [clue_service_1.ClueService, aurelia_router_1.Router])
-        ], Clues);
-        return Clues;
-    }());
-    exports.Clues = Clues;
-});
-
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 define('dashboard',["require", "exports", 'aurelia-framework', 'aurelia-router', './clue-service'], function (require, exports, aurelia_framework_1, aurelia_router_1, clue_service_1) {
     "use strict";
     var Dashboard = (function () {
@@ -294,7 +258,7 @@ define('dashboard',["require", "exports", 'aurelia-framework', 'aurelia-router',
         }
         Dashboard.prototype.created = function () {
             var _this = this;
-            this.clueService.choose(6).then(function (categories) { _this.categories = categories; });
+            return this.clueService.choose(6).then(function (categories) { _this.categories = categories; });
         };
         Dashboard.prototype.gotoDetail = function (clue) {
             this.router.navigateToRoute('clueDetail', { id: clue.id });
@@ -407,7 +371,7 @@ define('main',["require", "exports", './environment'], function (require, export
         if (environment_1.default.testing) {
             aurelia.use.plugin('aurelia-testing');
         }
-        aurelia.start().then(function () { return aurelia.setRoot(); });
+        return aurelia.start().then(function () { return aurelia.setRoot(); });
     }
     exports.configure = configure;
 });
@@ -465,8 +429,8 @@ define('resources/index',["require", "exports"], function (require, exports) {
 define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./app.css\"></require>\n  <router-view></router-view>\n</template>\n"; });
 define('text!app.css', ['module'], function(module) { module.exports = "h1 {\n  font-size: 1.2em;\n  color: #999;\n  margin-bottom: 0; }\n\nh2 {\n  font-size: 2em;\n  margin-top: 0;\n  padding-top: 0; }\n\nnav a {\n  padding: 5px 10px;\n  text-decoration: none;\n  margin-top: 10px;\n  display: inline-block;\n  background-color: #eee;\n  border-radius: 4px; }\n\nnav a:visited, a:link {\n  color: #607D8B; }\n\nnav a:hover {\n  color: #039be5;\n  background-color: #CFD8DC; }\n\nnav a.router-link-active {\n  color: #039be5; }\n"; });
 define('text!clue-detail.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./clue-detail.css\"></require>\n  <div if.bind=\"clue\">\n    <h2>Clue details!</h2>\n    <div><label>ID: </label>${clue.id}</div>\n    <label>Level: <input value.bind=\"clue.level\" placeholder=\"level\"></label>\n    <label>Clue: <input value.bind=\"clue.clue\" placeholder=\"clue\"></label>\n    <label>Answer: <input value.bind=\"clue.answer\" placeholder=\"answer\"></label>\n  </div>\n  <button click.trigger=\"goBack()\">Back</button>\n</template>\n"; });
+define('text!clues.html', ['module'], function(module) { module.exports = "<template>\n  delete me\n  <require from=\"./clues.css\"></require>\n  <h2>Clues</h2>\n  <ul class=\"clues\">\n    <li repeat.for=\"clue of clues\" class.bind=\"clue === selectedClue ? 'selected' : ''\">\n      <div>ID: ${clue.id}</div>\n      <div>Level: ${clue.level}</div>\n      <div>Clue: ${clue.clue}</div>\n      <div>Answer: ${clue.answer}</div>\n      <div><button click.trigger=\"onSelect(clue)\">Edit</button></div>\n    </li>\n  </ul>\n  <div if.bind=\"selectedClue\">\n    <h2>\n      ${selectedClue.clue} is my clue\n    </h2>\n    <button click.trigger=\"gotoDetail()\">View Details</button>\n  </div>\n</template>\n"; });
 define('text!clue-detail.css', ['module'], function(module) { module.exports = "label {\n  display: inline-block;\n  width: 3em;\n  margin: .5em 0;\n  color: #607D8B;\n  font-weight: bold; }\n\ninput {\n  height: 2em;\n  font-size: 1em;\n  padding-left: .4em; }\n\nbutton {\n  margin-top: 20px;\n  font-family: Arial;\n  background-color: #eee;\n  border: none;\n  padding: 5px 10px;\n  border-radius: 4px;\n  cursor: pointer;\n  cursor: hand; }\n\nbutton:hover {\n  background-color: #cfd8dc; }\n\nbutton:disabled {\n  background-color: #eee;\n  color: #ccc;\n  cursor: auto; }\n"; });
-define('text!clues.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./clues.css\"></require>\n  <h2>Clues</h2>\n  <ul class=\"clues\">\n    <li repeat.for=\"clue of clues\" class.bind=\"clue === selectedClue ? 'selected' : ''\">\n      <div>ID: ${clue.id}</div>\n      <div>Level: ${clue.level}</div>\n      <div>Clue: ${clue.clue}</div>\n      <div>Answer: ${clue.answer}</div>\n      <div><button click.trigger=\"onSelect(clue)\">Edit</button></div>\n    </li>\n  </ul>\n  <div if.bind=\"selectedClue\">\n    <h2>\n      ${selectedClue.clue} is my clue\n    </h2>\n    <button click.trigger=\"gotoDetail()\">View Details</button>\n  </div>\n</template>\n"; });
 define('text!clues.css', ['module'], function(module) { module.exports = ".selected {\n  background-color: #CFD8DC !important;\n  color: white; }\n\n.clues {\n  margin: 0 0 2em 0;\n  list-style-type: none;\n  padding: 0;\n  width: 15em; }\n\n.clues li {\n  cursor: pointer;\n  position: relative;\n  left: 0;\n  background-color: #EEE;\n  margin: .5em;\n  padding: .3em 0;\n  border-radius: 4px; }\n\n.clues li label {\n  display: block; }\n\n.clues li.selected:hover {\n  background-color: #BBD8DC !important;\n  color: white; }\n\n.clues li:hover {\n  color: #607D8B;\n  background-color: #DDD;\n  left: .1em; }\n\n.clues .text {\n  position: relative;\n  top: -3px; }\n\n.clues .badge {\n  display: inline-block;\n  font-size: small;\n  color: white;\n  padding: 0.8em 0.7em 0 0.7em;\n  background-color: #607D8B;\n  line-height: 1em;\n  position: relative;\n  left: -1px;\n  top: -4px;\n  height: 1.8em;\n  margin-right: .8em;\n  border-radius: 4px 0 0 4px; }\n"; });
 define('text!dashboard.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./dashboard.css\"></require>\n  <h3>Top Clues</h3>\n  <table border=\"1\">\n    <thead>\n      <tr>\n        <th repeat.for=\"category of categories\">${category.name}</th>\n      </tr>\n    </thead>\n    <tbody>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[0])\">${category.clues[0].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[1])\">${category.clues[1].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[2])\">${category.clues[2].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[3])\">${category.clues[3].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[4])\">${category.clues[4].clue}</td>\n      </tr>\n    </tbody>\n  </table>\n</template>\n"; });
 define('text!dashboard.css', ['module'], function(module) { module.exports = "[class*='col-'] {\n  float: left; }\n\n*, *:after, *:before {\n  -webkit-box-sizing: border-box;\n  -moz-box-sizing: border-box;\n  box-sizing: border-box; }\n\nh3 {\n  text-align: center;\n  margin-bottom: 0; }\n\n[class*='col-'] {\n  padding-right: 20px;\n  padding-bottom: 20px; }\n\n[class*='col-']:last-of-type {\n  padding-right: 0; }\n\n.grid {\n  margin: 0; }\n\n.col-1-4 {\n  width: 25%; }\n\n.module {\n  padding: 20px;\n  text-align: center;\n  color: #eee;\n  max-height: 120px;\n  min-width: 120px;\n  background-color: #607D8B;\n  border-radius: 2px; }\n\nh4 {\n  position: relative; }\n\n.module:hover {\n  background-color: #EEE;\n  cursor: pointer;\n  color: #607d8b; }\n\n.grid-pad {\n  padding: 10px 0; }\n\n.grid-pad > [class*='col-']:last-of-type {\n  padding-right: 20px; }\n\n@media (max-width: 600px) {\n  .module {\n    font-size: 10px;\n    max-height: 75px; } }\n\n@media (max-width: 1024px) {\n  .grid {\n    margin: 0; }\n  .module {\n    min-width: 60px; } }\n"; });
