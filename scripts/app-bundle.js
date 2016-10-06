1f71d9bd6aa10719d02b464ad74903e594b8a536
Eric Heikes <eheikes@gmail.com>
2016-10-05 22:18:12 -0500

    Rebuild the app & vendor bundles.

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -167,6 +167,42 @@ define('clue-service',["require", "exports", 'aurelia-framework', 'aurelia-fetch
     exports.ClueService = ClueService;
 });
 
+define('player',["require", "exports"], function (require, exports) {
+    "use strict";
+    var Player = (function () {
+        function Player(name) {
+            this.name = name;
+            this.money = 0;
+        }
+        return Player;
+    }());
+    exports.Player = Player;
+});
+
+define('game-state',["require", "exports"], function (require, exports) {
+    "use strict";
+    var GameRound = (function () {
+        function GameRound() {
+            this.number = 0;
+            this.name = 'J!Party';
+        }
+        return GameRound;
+    }());
+    exports.GameRound = GameRound;
+    var GameState = (function () {
+        function GameState() {
+        }
+        GameState.prototype.reset = function (players, categories) {
+            this.players = players;
+            this.currentPlayer = this.players[0];
+            this.categories = categories;
+            this.round = new GameRound();
+        };
+        return GameState;
+    }());
+    exports.GameState = GameState;
+});
+
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -176,11 +212,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-define('app',["require", "exports", 'aurelia-framework', './clue-service'], function (require, exports, aurelia_framework_1, clue_service_1) {
+define('app',["require", "exports", 'aurelia-framework', './clue-service', './game-state', './player'], function (require, exports, aurelia_framework_1, clue_service_1, game_state_1, player_1) {
     "use strict";
     var App = (function () {
-        function App(clueService) {
+        function App(clueService, gameState) {
             this.clueService = clueService;
+            this.gameState = gameState;
             this.title = 'J!Party';
         }
         App.prototype.configureRouter = function (config, router) {
@@ -193,14 +230,23 @@ define('app',["require", "exports", 'aurelia-framework', './clue-service'], func
             this.router = router;
         };
         App.prototype.created = function () {
+            var _this = this;
             console.log('Loading clues...');
-            this.clueService.loadClues().then(function () {
+            return this.clueService.loadClues().then(function () {
                 console.log('clues loaded');
+                return _this.clueService.choose(6);
+            }).then(function (categories) {
+                var players = [
+                    new player_1.Player('You'),
+                    new player_1.Player('Computer 1'),
+                    new player_1.Player('Computer 2'),
+                ];
+                _this.gameState.reset(players, categories);
             }).catch(function (err) { console.log('ERROR!', err); });
         };
         App = __decorate([
-            aurelia_framework_1.inject(clue_service_1.ClueService), 
-            __metadata('design:paramtypes', [clue_service_1.ClueService])
+            aurelia_framework_1.inject(clue_service_1.ClueService, game_state_1.GameState), 
+            __metadata('design:paramtypes', [clue_service_1.ClueService, game_state_1.GameState])
         ], App);
         return App;
     }());
@@ -216,24 +262,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-define('board',["require", "exports", 'aurelia-framework', 'aurelia-router', './clue-service'], function (require, exports, aurelia_framework_1, aurelia_router_1, clue_service_1) {
+define('board',["require", "exports", 'aurelia-framework', 'aurelia-router', './game-state'], function (require, exports, aurelia_framework_1, aurelia_router_1, game_state_1) {
     "use strict";
     var Board = (function () {
-        function Board(clueService, router) {
-            this.clueService = clueService;
+        function Board(gameState, router) {
+            this.gameState = gameState;
             this.router = router;
-            this.categories = [];
         }
         Board.prototype.created = function () {
-            var _this = this;
-            return this.clueService.choose(6).then(function (categories) { _this.categories = categories; });
+            this.categories = this.gameState.categories;
+            this.currentPlayer = this.gameState.currentPlayer;
+            this.players = this.gameState.players;
+            this.round = this.gameState.round;
         };
         Board.prototype.gotoDetail = function (clue) {
             this.router.navigateToRoute('clueDetail', { id: clue.id });
         };
         Board = __decorate([
-            aurelia_framework_1.inject(clue_service_1.ClueService, aurelia_router_1.Router), 
-            __metadata('design:paramtypes', [clue_service_1.ClueService, aurelia_router_1.Router])
+            aurelia_framework_1.inject(game_state_1.GameState, aurelia_router_1.Router), 
+            __metadata('design:paramtypes', [game_state_1.GameState, aurelia_router_1.Router])
         ], Board);
         return Board;
     }());
@@ -278,16 +325,6 @@ define('environment',["require", "exports"], function (require, exports) {
         debug: true,
         testing: true
     };
-});
-
-define('game-state',["require", "exports"], function (require, exports) {
-    "use strict";
-    var GameState = (function () {
-        function GameState() {
-        }
-        return GameState;
-    }());
-    exports.GameState = GameState;
 });
 
 define('input',["require", "exports"], function (require, exports) {
@@ -427,9 +464,9 @@ define('resources/index',["require", "exports"], function (require, exports) {
 
 define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./app.css\"></require>\n  <router-view></router-view>\n</template>\n"; });
 define('text!app.css', ['module'], function(module) { module.exports = "h1 {\n  font-size: 1.2em;\n  color: #999;\n  margin-bottom: 0; }\n\nh2 {\n  font-size: 2em;\n  margin-top: 0;\n  padding-top: 0; }\n\nnav a {\n  padding: 5px 10px;\n  text-decoration: none;\n  margin-top: 10px;\n  display: inline-block;\n  background-color: #eee;\n  border-radius: 4px; }\n\nnav a:visited, a:link {\n  color: #607D8B; }\n\nnav a:hover {\n  color: #039be5;\n  background-color: #CFD8DC; }\n\nnav a.router-link-active {\n  color: #039be5; }\n"; });
-define('text!board.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./board.css\"></require>\n  <h3>Top Clues</h3>\n  <table border=\"1\">\n    <thead>\n      <tr>\n        <th repeat.for=\"category of categories\">${category.name}</th>\n      </tr>\n    </thead>\n    <tbody>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[0])\">${category.clues[0].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[1])\">${category.clues[1].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[2])\">${category.clues[2].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[3])\">${category.clues[3].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[4])\">${category.clues[4].clue}</td>\n      </tr>\n    </tbody>\n  </table>\n</template>\n"; });
-define('text!clue-detail.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./clue-detail.css\"></require>\n  <div if.bind=\"clue\">\n    <h2>Clue details!</h2>\n    <div><label>ID: </label>${clue.id}</div>\n    <label>Level: <input value.bind=\"clue.level\" placeholder=\"level\"></label>\n    <label>Clue: <input value.bind=\"clue.clue\" placeholder=\"clue\"></label>\n    <label>Answer: <input value.bind=\"clue.answer\" placeholder=\"answer\"></label>\n  </div>\n  <button click.trigger=\"goBack()\">Back</button>\n</template>\n"; });
-define('text!title-screen.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"title-screen\">\n    <h1>J!Party</h1>\n    <p if.bind=\"loading\">Loading...</p>\n    <p if.bind=\"!loading\" class=\"press-any\">Press any key to continue</p>\n    <p class=\"legalese\">\n      Copyright © 2016 Eric Heikes and licensed under the Apache 2.0 License.\n      The Jeopardy! game show and all elements thereof, including but not limited to copyright and trademark thereto, are the property of Jeopardy Productions, Inc. and are protected under law. This software is not affiliated with, sponsored by, or operated by Jeopardy Productions, Inc.\n    </p>\n  </div>\n</template>\n"; });
+define('text!board.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./board.css\"></require>\n  <h3>${round.name}</h3>\n  <table border=\"1\">\n    <thead>\n      <tr>\n        <th repeat.for=\"category of categories\">${category.name}</th>\n      </tr>\n    </thead>\n    <tbody>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[0])\">${category.clues[0].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[1])\">${category.clues[1].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[2])\">${category.clues[2].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[3])\">${category.clues[3].clue}</td>\n      </tr>\n      <tr>\n        <td repeat.for=\"category of categories\" click.trigger=\"gotoDetail(category.clues[4])\">${category.clues[4].clue}</td>\n      </tr>\n    </tbody>\n  </table>\n  <p class=\"please-choose\">${currentPlayer.name}, choose a category.</p>\n  <ul class=\"players\">\n    <li repeat.for=\"player of players\">${player.name} $${player.money}</li>\n  </ul>\n</template>\n"; });
 define('text!board.css', ['module'], function(module) { module.exports = "[class*='col-'] {\n  float: left; }\n\n*, *:after, *:before {\n  -webkit-box-sizing: border-box;\n  -moz-box-sizing: border-box;\n  box-sizing: border-box; }\n\nh3 {\n  text-align: center;\n  margin-bottom: 0; }\n\n[class*='col-'] {\n  padding-right: 20px;\n  padding-bottom: 20px; }\n\n[class*='col-']:last-of-type {\n  padding-right: 0; }\n\n.grid {\n  margin: 0; }\n\n.col-1-4 {\n  width: 25%; }\n\n.module {\n  padding: 20px;\n  text-align: center;\n  color: #eee;\n  max-height: 120px;\n  min-width: 120px;\n  background-color: #607D8B;\n  border-radius: 2px; }\n\nh4 {\n  position: relative; }\n\n.module:hover {\n  background-color: #EEE;\n  cursor: pointer;\n  color: #607d8b; }\n\n.grid-pad {\n  padding: 10px 0; }\n\n.grid-pad > [class*='col-']:last-of-type {\n  padding-right: 20px; }\n\n@media (max-width: 600px) {\n  .module {\n    font-size: 10px;\n    max-height: 75px; } }\n\n@media (max-width: 1024px) {\n  .grid {\n    margin: 0; }\n  .module {\n    min-width: 60px; } }\n"; });
+define('text!clue-detail.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"./clue-detail.css\"></require>\n  <div if.bind=\"clue\">\n    <h2>Clue details!</h2>\n    <div><label>ID: </label>${clue.id}</div>\n    <label>Level: <input value.bind=\"clue.level\" placeholder=\"level\"></label>\n    <label>Clue: <input value.bind=\"clue.clue\" placeholder=\"clue\"></label>\n    <label>Answer: <input value.bind=\"clue.answer\" placeholder=\"answer\"></label>\n  </div>\n  <button click.trigger=\"goBack()\">Back</button>\n</template>\n"; });
 define('text!clue-detail.css', ['module'], function(module) { module.exports = "label {\n  display: inline-block;\n  width: 3em;\n  margin: .5em 0;\n  color: #607D8B;\n  font-weight: bold; }\n\ninput {\n  height: 2em;\n  font-size: 1em;\n  padding-left: .4em; }\n\nbutton {\n  margin-top: 20px;\n  font-family: Arial;\n  background-color: #eee;\n  border: none;\n  padding: 5px 10px;\n  border-radius: 4px;\n  cursor: pointer;\n  cursor: hand; }\n\nbutton:hover {\n  background-color: #cfd8dc; }\n\nbutton:disabled {\n  background-color: #eee;\n  color: #ccc;\n  cursor: auto; }\n"; });
+define('text!title-screen.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"title-screen\">\n    <h1>J!Party</h1>\n    <p if.bind=\"loading\">Loading...</p>\n    <p if.bind=\"!loading\" class=\"press-any\">Press any key to continue</p>\n    <p class=\"legalese\">\n      Copyright © 2016 Eric Heikes and licensed under the Apache 2.0 License.\n      The Jeopardy! game show and all elements thereof, including but not limited to copyright and trademark thereto, are the property of Jeopardy Productions, Inc. and are protected under law. This software is not affiliated with, sponsored by, or operated by Jeopardy Productions, Inc.\n    </p>\n  </div>\n</template>\n"; });
 //# sourceMappingURL=app-bundle.js.map
