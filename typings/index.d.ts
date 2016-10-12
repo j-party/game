64df5b4965fb1dcee56a34c4705ae68c2011121f
Eric Heikes <eheikes@gmail.com>
2016-10-11 20:38:34 -0500

    Add Mousetrap library to the project.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/angular-protractor/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/mousetrap/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/random-js/index.d.ts" />
 /// <reference path="globals/selenium-webdriver/index.d.ts" />
